b6081a8b26fd6d229bdbdfc45c2e4d37fb3112d4
Gustavo Farias <gugafarias2009@gmail.com>
2021-10-14 23:00:48 -0300

    feat: Splash screen and Sign In screen

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  TouchableOpacityProps
+} from "react-native";
+
+import DiscordImg from '../../assets/discord.png';
+import { styles } from "./styles";
+
+type ButtonIconProps = TouchableOpacityProps & {
+  title: string;
+}
+
+export function ButtonIcon({title, ...rest}: ButtonIconProps) {
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <View style={styles.iconWrapper}>
+        <Image source={DiscordImg} style={styles.icon}/>
+      </View>
+
+      <Text style={styles.title}>
+        { title }
+      </Text>
+    </TouchableOpacity>
+  );
+}
